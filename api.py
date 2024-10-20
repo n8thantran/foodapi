dbc83df66e32c4e3ef567190b20d40f5ac403cbe
Nathan Tran <180703202+n8thantran@users.noreply.github.com>
2024-10-19 19:41:24 -0700

    Update api.py

--- api.py
+++ api.py
@@ -21,7 +21,14 @@ recipes = {
             "Mix wet ingredients.",
             "Combine and fold in blueberries.",
             "Bake for 20-25 minutes."
-        ]
+        ],
+        "nutrition": {
+            "calories": 350,
+            "sugar": "20g",
+            "protein": "5g",
+            "total_fats": "15g",
+            "sodium": "200mg"
+        }
     },
     "chocolate_muffin": {
         "ingredients": [
@@ -40,7 +47,14 @@ recipes = {
             "Mix wet ingredients.",
             "Combine.",
             "Bake for 20-25 minutes."
-        ]
+        ],
+        "nutrition": {
+            "calories": 400,
+            "sugar": "25g",
+            "protein": "6g",
+            "total_fats": "18g",
+            "sodium": "220mg"
+        }
     },
     "banana_bread": {
         "ingredients": [
@@ -60,7 +74,14 @@ recipes = {
             "Mix wet ingredients.",
             "Combine.",
             "Bake for 20-25 minutes."
-        ]
+        ],
+        "nutrition": {
+            "calories": 300,
+            "sugar": "18g",
+            "protein": "4g",
+            "total_fats": "12g",
+            "sodium": "180mg"
+        }
     }
 }
 
@@ -74,4 +95,4 @@ async def get_baked_good(item_type: str):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='nathanapi.clowdertech.com', port=8000)
